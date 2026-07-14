82cca78675d8144ec13c1efb6de6e42c6fe4a637
Gayatri Mareedu <gayatrimareedu9@gmail.com>
2026-07-14 19:38:01 +0530

    Complete image loading and install face recognition

--- attendanceProject.py
+++ attendanceProject.py
@@ -1,2 +1,25 @@
-print("Hello, World!")
+import cv2
+import os
 
+path = "ImagesAttendance"
+
+myList = os.listdir(path)
+
+print(myList)
+
+images = []
+
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    imgRGB = cv2.cvtColor(curImg, cv2.COLOR_BGR2RGB)
+    images.append(curImg)
+
+print(len(images))   
+
+print(type(images[0]))
+
+for img in images:
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
